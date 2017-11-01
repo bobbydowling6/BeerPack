e9b55452be9eb53b3955db778a80d5434b6b8360
Bob Dowling <bobbydowling6@gmail.com>
2017-10-31 18:39:15 -0700

    Made changes

---
+++ BeerPack/Controllers/AleController.cs
@@ -0,0 +1,61 @@
+﻿using BeerPack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeerPack.Controllers
+{
+    public class AleController : Controller
+    {
+
+        public ActionResult List()
+        {
+            List<Ales> ales = new List<Ales>();
+            ales.Add(new Ales
+            {
+                ID = 1,
+                Name = "Shocktop",
+                Price = 3,
+                Description = "Belgian White Ale",
+                Image = "/images/Ales/shocktop.jpg"
+            });
+
+            ales.Add(new Ales
+            {
+                ID = 2,
+                Name = "Goose Island 312",
+                Price = 3,
+                Description = "Wheat Ale",
+                Image = "images/Ales/gooseisland312.png"
+            });
+            return View(ales);
+        }
+        // GET: Ale
+        public ActionResult Index(int id)
+        {
+            var ales = new Models.Ales();
+            if(id == 1)
+            {
+                ales.Name = "Shocktop";
+                ales.Description = "Belgian White Ale";
+                ales.Price = 3;
+                ales.Image = "images/Ales/shocktop.jpg";
+            }
+            else if (id == 2)
+            {
+                ales.Name = "Goose Island 312";
+                ales.Description = "Wheat Ale";
+                ales.Price = 3;
+                ales.Image = "images/Ales/gooseisland312.png";
+            }else
+            {
+                return HttpNotFound("This product doesn't exist");
+            }
+            
+
+            return View(ales);
+        }
+    }
+}
--- BeerPack/Controllers/BeerController.cs
+++ BeerPack/Controllers/BeerController.cs
@@ -6,14 +6,27 @@ using System.Web.Mvc;
 
 namespace BeerPack.Controllers
 {
+
     public class BeerController : Controller
     {
         // GET: Beer
         public ActionResult Index(string id)
         {
+            var model = new BeerPack.Models.Ales();
             return Content(id);
         }
 
-        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string firstName, string lastName, 
+            string yourEmail, int yourPhone, string yourComments)
+        {
+            return Content("Thanks for properly filling out this form, we will contact " +
+                "you very shortly to help answer all of your questions and concerns.");
+        }
     }
 }
