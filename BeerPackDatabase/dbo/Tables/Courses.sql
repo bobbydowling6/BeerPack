79a28bbaafd52cebae3a74cd74a61676d637fa1e
Bob Dowling <bobbydowling6@gmail.com>
2017-11-08 09:25:38 -0800

    Added a database folder

---
+++ BeerPackDatabase/dbo/Tables/Courses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Courses] (
+    [Name]        NVARCHAR (100)  NOT NULL,
+    [Description] NVARCHAR (1000) NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ([Name] ASC)
+);
+
