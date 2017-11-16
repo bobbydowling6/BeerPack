4623a615209f74257940e65cb2f60cf0b48838f5
Bob Dowling <bobbydowling6@gmail.com>
2017-11-16 14:51:31 -0800

    Added a registration page for users to log in and setup a database to save user info. Also added the AccountController, the StartUp class, and new SQL tables for user registration info. Edited the layout page, the web configuration, the BeerPack project solution, and BeerPack Database

---
+++ BeerPack/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeerPack.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<UserManager<IdentityUser>>();
+            }
+        }
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LogOn()
+        {
+            HttpContext.GetOwinContext().Authentication.SignIn();
+            return View();
+        }
+        public ActionResult LogOff()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string password)
+        {
+            //Make sure the following statements are in the using block:
+            //using Microsoft.AspNet.Identity;
+            //using Microsoft.AspNet.Identity.EntityFramework;
+            //using Microsoft.AspNet.Identity.Owin;
+            var userManager = HttpContext.GetOwinContext().GetUserManager<UserManager<IdentityUser>>();
+            IdentityUser user = new IdentityUser { Email = username, UserName = username };
+
+            IdentityResult result = userManager.Create(user, password);
+            if (result.Succeeded)
+            {
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                HttpContext.GetOwinContext().Authentication.SignIn(new Microsoft.Owin.Security.AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTime.UtcNow.AddDays(7)
+                }, userIdentity);
+
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = result.Errors;
+            return View();
+        }
+    }
+}
--- BeerPackDatabase/dbo/Tables/Order.sql
+++ BeerPackDatabase/dbo/Tables/Order.sql
@@ -1,18 +1,19 @@
-﻿CREATE TABLE [dbo].[Order]
-(
-	[Id] INT IDENTITY NOT NULL, 
-    [TrackingNumber] CHAR(8) NOT NULL DEFAULT left(newid(), 8), 
-    [Email] NVARCHAR(512) NOT NULL, 
-    [PurchaserName] NVARCHAR(512) NOT NULL, 
-    [ShippingAddress1] NVARCHAR(1000) NOT NULL, 
-    [ShippingCity] NVARCHAR(1000) NOT NULL, 
-    [ShippingState] NVARCHAR(100) NOT NULL, 
-    [ShippingPostalCode] NCHAR(10) NOT NULL, 
-    [SubTotal] MONEY NOT NULL, 
-    [ShippingAndHandling] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL, 
-    [DateCreated] DATETIME NOT NULL DEFAULT GetDate(), 
-    [DateLastModified] DATETIME NOT NULL DEFAULT GetDate(), 
-    [ShipDate] DATETIME NULL, 
-    CONSTRAINT [PK_Order] PRIMARY KEY ([Id])
-)
+﻿CREATE TABLE [dbo].[Order] (
+    [Id]                  INT             IDENTITY (1, 1) NOT NULL,
+    [TrackingNumber]      CHAR (8)        DEFAULT (left(newid(),(8))) NOT NULL,
+    [Email]               NVARCHAR (512)  NOT NULL,
+    [PurchaserName]       NVARCHAR (512)  NOT NULL,
+    [ShippingAddress1]    NVARCHAR (1000) NOT NULL,
+    [ShippingCity]        NVARCHAR (1000) NOT NULL,
+    [ShippingState]       NVARCHAR (100)  NOT NULL,
+    [ShippingPostalCode]  NCHAR (10)      NOT NULL,
+    [SubTotal]            MONEY           NOT NULL,
+    [ShippingAndHandling] MONEY           NOT NULL,
+    [Tax]                 MONEY           NOT NULL,
+    [DateCreated]         DATETIME        DEFAULT (getdate()) NOT NULL,
+    [DateLastModified]    DATETIME        DEFAULT (getdate()) NOT NULL,
+    [ShipDate]            DATETIME        NULL,
+    CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
