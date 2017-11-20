232592049459bf39e7f13f839c4a7f282bec8f98
Bob Dowling <bobbydowling6@gmail.com>
2017-11-20 13:19:51 -0800

    Updated beerscript data, account controller, and signin view. Added an account index view

--- BeerPackDatabase/BeerScript.Data.sql
+++ BeerPackDatabase/BeerScript.Data.sql
@@ -16,12 +16,12 @@ insert into Beer ([Name], [Brand], [Beer Style], [Description], Price, Image)val
 ('Bells Best Brown Ale', 'Bells Brewery, inc.', 'Ale', 'Brown Ale' , 2, '/images/Ales/bells-best-brown-ale.jpg'),
 ('Big Wave', 'Kona Brewing Co.', 'Ale', 'Golden Ale', 2, '/images/Ales/kona-beer.png'),
 ('Shiner Bock Birthday Beer', 'The Gambrinus Company ', 'Ale', 'Coffee Ale', 2, '/images/Ales/SHiner-793x525.png'),
-('Union Jack IPA', 'Firestone Walker Brewing Co.', 'IPA', 'American IPA', 2, '/images/IPA^s/firestoneipa.jpg'),
-('Goose Island IPA', 'Goose Island Brewing Co.', 'IPA', 'English IPA', 2, '/images/IPA^s/gooseislandipa.jpg'),
-('Lagunitas IPA', 'Lagunitas Brewing Co.', 'IPA', 'American IPA', 2, '/images/IPA^s/Lagunitas.jpg'),
-('Samual Adams Rebel IPA', 'The Boston Beer Company', 'IPA', 'American IPA', 2, '/images/IPA^s/samualadamsipa.jpg'),
-('Sierra Nevada Pale Ale', 'Sierra Nevada Brewing Co.', 'IPA', 'American Pale Ale', 2, '/images/IPA^s/sierranevada.jpg'),
-('VooDoo Ranger IPA', 'New Belgium Brewing Co.', 'IPA', 'American IPA', 2, '/images/Ipa^s/voodoo-ranger-by-new-belgium-brewing-co.jpg'),
+('Union Jack IPA', 'Firestone Walker Brewing Co.', 'IPA', 'American IPA', 2, '/images/IPAs/firestoneipa.jpg'),
+('Goose Island IPA', 'Goose Island Brewing Co.', 'IPA', 'English IPA', 2, '/images/IPAs/gooseislandipa.jpg'),
+('Lagunitas IPA', 'Lagunitas Brewing Co.', 'IPA', 'American IPA', 2, '/images/IPAs/Lagunitas.jpg'),
+('Samual Adams Rebel IPA', 'The Boston Beer Company', 'IPA', 'American IPA', 2, '/images/IPAs/samualadamsipa.jpg'),
+('Sierra Nevada Pale Ale', 'Sierra Nevada Brewing Co.', 'IPA', 'American Pale Ale', 2, '/images/IPAs/sierranevada.jpg'),
+('VooDoo Ranger IPA', 'New Belgium Brewing Co.', 'IPA', 'American IPA', 2, '/images/Ipas/voodoo-ranger-by-new-belgium-brewing-co.jpg'),
 ('LongBoard', 'Kona Brewing Co.', 'Lager', 'Island Lager', 2, '/images/Lagers/beer-longboard-lager.jpg'),
 ('Hop House 13 Lager', 'Guinness Ltd.', 'Lager', 'Euro Pale Lager', 2, '/images/Lagers/guinness-guinness-hop-house-13-lager_1478111845.png'),
 ('Eliot Ness Amber Lager', 'Great Lakes Brewing Co.', 'Lager', 'Amber Lager', 2, '/images/Lagers/Lager-Beer-Gear-Patrol-Great-Lakes.jpg'),
@@ -40,3 +40,15 @@ insert into Beer ([Name], [Brand], [Beer Style], [Description], Price, Image)val
 ('Hofbräu Winter Spezial', 'Hofbräu Winter Spezial', 'Porter', 'German Porter', 2, '/images/Porters/Hofbrau winter spezial.png'),
 ('Stone Smoked Porter W/Vanilla Bean', 'Stone Brewing', 'Porter', 'Smoked Beer', 2, '/images/Porters/stone-smoked-porter-vanilla-bean.jpg'),
 ('Yuengling Dark Brewed Porter', 'Yuengling Brewery', 'Porter', 'American Porter', 2, '/images/Porters/Yuengling-Porter.jpg')
+
+SELECT * FROM Beer
+
+Update Beer set Image = '/images/IPAs/firestoneipa.jpg' where Image = '/images/IPAs/firestoneipa.jpg';
+Update Beer set Image = '/images/IPAs/gooseislandipa.jpg' where Image = '/images/IPAs/gooseislandipa.jpg';
+Update Beer set Image = '/images/IPAs/Lagunitas.jpg' where Image = '/images/IPAs/Lagunitas.jpg';
+
+Update Beer set Image = '/images/IPAs/samualadamsipa.jpg' where Image = '/images/IPAs/samualadamsipa.jpg';
+Update Beer set Image = '/images/IPAs/sierranevada.jpg' where Image = '/images/IPAs/sierranevada.jpg';
+Update Beer set Image = '/images/IPAs/voodoo-ranger-by-new-belgium-brewing-co.jpg' where Image = '/images/IPAs/voodoo-ranger-by-new-belgium-brewing-co.jpg';
+
+Update Beer set Image = '/images/Lagers/samual adams.jpg' where Image = '/images/Lagers/samual adams.jpg';
